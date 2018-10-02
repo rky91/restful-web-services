3e67522848549f94437ab8d19375463d1a025bd1
rky91 <rakeshkumar.jena91@gmail.com>
2018-10-03 00:08:59 +0530

    Added swagger for API documentation

--- restful-web-services/src/main/java/com/rky/restfulwebservice/beans/User.java
+++ restful-web-services/src/main/java/com/rky/restfulwebservice/beans/User.java
@@ -5,13 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
 public class User {
 	
 	private int id;
 	
 	@Size(min=2, message="name should have atleast 2 characters.")
+	@ApiModelProperty(notes="name should have atleast 2 characters.")
 	private String name;
+	
 	@Past
+	@ApiModelProperty(notes="Birth date should be in past.")
 	private Date birthDate;
 	
 	public User() {}
