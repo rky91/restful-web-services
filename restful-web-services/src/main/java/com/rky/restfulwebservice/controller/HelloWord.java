57c701eb00404793f78de4f3b43a3bb5bb1cf694
rky91 <rakeshkumar.jena91@gmail.com>
2018-10-02 19:11:37 +0530

    Internationalization part-1

--- restful-web-services/src/main/java/com/rky/restfulwebservice/controller/HelloWord.java
+++ restful-web-services/src/main/java/com/rky/restfulwebservice/controller/HelloWord.java
@@ -1,15 +1,27 @@
 package com.rky.restfulwebservice.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWord {
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping("/helloworld")
 	public String getHelloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/hello-world-int")
+	public String helloWorldInternationalized(@RequestHeader(value="Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("morning.message", null, locale);
 	}
 	
 	
